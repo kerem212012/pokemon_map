06d1bd490c27f7fec645cedecf12101b14a29024
kerem212012 <rockstarfoxykerem@gmail.com>
2025-01-29 11:08:57 +0300

    Add relation to db models

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,6 +1,9 @@
-from pokemon_entities.models import Pokemon
+from pokemon_entities.models import Pokemon,PokemonEntity
 from django.contrib import admin
 
 @admin.register(Pokemon)
 class PokemonAdmin(admin.ModelAdmin):
+    pass
+@admin.register(PokemonEntity)
+class PokemonEntityAdmin(admin.ModelAdmin):
     pass
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,5 +3,16 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(blank=True)
+
     def __str__(self):
         return f"{self.title}"
+
+class PokemonEntity(models.Model):
+    Pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    Lat = models.FloatField()
+    Lon = models.FloatField()
+    
+
+
+    def __str__(self):
+        return f"{self.Lat} , {self.Lon}"
