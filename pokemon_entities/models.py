cbbf711e1d22827178ba4a9bf7e70adb4a702c53
kerem212012 <rockstarfoxykerem@gmail.com>
2025-02-04 18:08:00 +0300

    title another languages on info page

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,8 @@ class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(blank=True)
     description = models.TextField(blank=True)
+    title_en = models.CharField(max_length=200,blank=True)
+    title_jp = models.CharField(max_length=200,blank=True)
 
     def __str__(self):
         return f"{self.title}"
