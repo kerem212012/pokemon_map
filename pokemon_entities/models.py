1a19e6d51b6b227e9308d4bd25acc2d47276f3c2
kerem212012 <rockstarfoxykerem@gmail.com>
2025-02-07 14:45:51 +0300

    add agreement option

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,7 +2,7 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(verbose_name="имя",max_length=200)
-    image = models.ImageField("картинка",blank=True)
+    image = models.ImageField(verbose_name="картинка")
     description = models.TextField(verbose_name="описание",blank=True)
     title_en = models.CharField(verbose_name="имя на английском",max_length=200,blank=True)
     title_jp = models.CharField(verbose_name="имя по японски",max_length=200,blank=True)
@@ -11,11 +11,11 @@ class Pokemon(models.Model):
         return f"{self.title}"
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon,verbose_name="покемон", on_delete=models.CASCADE,null=True)
+    pokemon = models.ForeignKey(Pokemon,verbose_name="покемон", on_delete=models.CASCADE)
     lat = models.FloatField(verbose_name="широта")
     lon = models.FloatField(verbose_name="долгота")
-    appeared_at = models.DateTimeField(verbose_name="появится в",blank=True,null=True)
-    disappeared_at = models.DateTimeField(verbose_name="исчезнет в",blank=True,null=True)
+    appeared_at = models.DateTimeField(verbose_name="появится в")
+    disappeared_at = models.DateTimeField(verbose_name="исчезнет в")
     level = models.IntegerField(verbose_name="уровень",blank=True,null=True)
     health = models.IntegerField(verbose_name="здоровые",blank=True,null=True)
     strength = models.IntegerField(verbose_name="сила",blank=True,null=True)
